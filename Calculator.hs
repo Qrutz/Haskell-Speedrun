a7e2d2a174c40d04a8732ae46ac88509461c9872
Qrutz <40356149+Qrutz@users.noreply.github.com>
2022-01-14 22:34:52 +0100

    Update Calculator.hs

--- Calculator.hs
+++ Calculator.hs
@@ -1,7 +1,7 @@
 -- This module is a starting point for implementing the Graph Drawing
 -- Calculator as described in Part II of the Standard Lab. You can use this
 -- directly, or just study it as an example of how to use threepenny-gui.
-
+z
 import ThreepennyPages
     ( mkButton,
       mkCanvas,
