5fcbb61d9c99524ee19d557b9e1f60009a2b2247
Qrutz <40356149+Qrutz@users.noreply.github.com>
2022-01-17 23:50:11 +0100

    Update ms.hs

--- ms.hs
+++ ms.hs
@@ -1,7 +1,7 @@
 import Data.Maybe (listToMaybe)
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' _ [] _ = []
+zipWith' _ [] _ = []aa
 zipWith' _ _ [] = []
 zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
 
