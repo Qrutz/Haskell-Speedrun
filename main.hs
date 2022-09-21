873e9f1e7a941a6e4f55dcf60afcabef53d1a003
Qrutz <40356149+Qrutz@users.noreply.github.com>
2022-09-21 18:28:41 +0200

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,173 +1,227 @@
-import Data.List
-import Data.Text (toUpper)
+module Sudoku where
 
-data Tree a = Leaf | Node a (Tree a) (Tree a)
-  deriving (Show)
+import Test.QuickCheck
+import Data.Char
+import Data.List(nub,elemIndices)
+import Data.Maybe
 
-treeDepth :: Tree Int -> Int -- ll --
-treeDepth Leaf = 0
-treeDepth (Node _ leftSubtree rightSubtree) = 1 + max (treeDepth leftSubtree) (treeDepth rightSubtree)
+------------------------------------------------------------------------------v
 
-singleTon :: a -> Tree a
-singleTon x = Node x Leaf Leaf
+-- | Representation of sudoku puzzles (allows some junk)
+type Cell = Maybe Int -- a single cell
+type Row  = [Cell]    -- a row is a list of cells
 
-treeInsert :: (Ord a) => a -> Tree a -> Tree a
-treeInsert x Leaf = singleTon x
-treeInsert x (Node a left right)
-  | x == a = Node x left right
-  | x < a = Node a (treeInsert x left) right
-  | x > a = Node a left (treeInsert x right)
+data Sudoku = Sudoku [Row]
+ deriving ( Show, Eq )
 
-exp2 :: Tree Int
-exp2 = Node 2 t1 (Node 1 t1 t0)
+rows :: Sudoku -> [Row]
+rows (Sudoku ms) = ms
+
+-- | A sample sudoku puzzle
+example :: Sudoku
+example =
+    Sudoku
+      [ [j 3,j 6,n  ,n  ,j 7,j 1,j 2,n  ,n  ]
+      , [n  ,j 5,n  ,n  ,n  ,n  ,j 1,j 8,n  ]
+      , [n  ,n  ,j 9,j 2,n  ,j 4,j 7,n  ,n  ]
+      , [n  ,n  ,n  ,n  ,j 1,j 3,n  ,j 2,j 8]
+      , [j 4,n  ,n  ,j 5,n  ,j 2,n  ,n  ,j 9]
+      , [j 2,j 7,n  ,j 4,j 6,n  ,n  ,n  ,n  ]
+      , [n  ,n  ,j 5,j 3,n  ,j 8,j 9,n  ,n  ]
+      , [n  ,j 8,j 3,n  ,n  ,n  ,n  ,j 6,n  ]
+      , [n  ,n  ,j 7,j 6,j 9,n  ,n  ,j 4,j 3]
+      ]
   where
-    t1 = Node 1 Leaf Leaf
-    t0 = Node 0 Leaf Leaf
+    n = Nothing
+    j = Just
 
-y = treeInsert 3 exp2
 
-arr :: [Int]
-arr = [1, 2, 2, 3, 4]
 
--- add to back
-add :: a -> [a] -> [a]
-add x (y : ys) = y : ys ++ [x]
+--example2 = Sudoku $ replicate 9 $ replicate 9 (Just 3)
 
--- add to front --
 
-add' :: a -> [a] -> [a]
-add' x (y : ys) = x : ys
+-- * A1
 
-lengthArr :: [a] -> Int
-lengthArr (x : xs) = 1 + lengthArr xs
+-- | allBlankSudoku is a sudoku with just blanks
+allBlankSudoku :: Sudoku
+allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 Nothing
 
-remove :: [Int] -> [Int]
-remove (x : xs) = xs
+-- * A2
 
-alreadyVisited :: Int -> [Int] -> Bool
-alreadyVisited x [] = False
-alreadyVisited x (v : visisted)
-  | x == v = True
-  | otherwise = alreadyVisited x visisted
+-- | isSudoku sud checks if sud is really a valid representation of a sudoku
+-- puzzle
+-- Checks number of rows, cell per row and the cell values
+isSudoku :: Sudoku -> Bool
+isSudoku sud = length (rows sud) == 9 && nub (map length $ rows sud) == [9] && not (any (`notElem` validInputs) (concatMap (take 9) $ rows sud))
+  where validInputs = Nothing:[Just m | m <- [ 1..9]]
 
-lol arr = length arr /= length (nub arr)
+-- * A3
 
-data Sudoku = Sudoku [[Int]]
+-- | isFilled sud checks if sud is completely filled in,
+-- i.e. there are no blanks
+isFilled :: Sudoku -> Bool
+isFilled sud = all isJust (concat (rows sud))
 
--- i --
-showSudoku (Sudoku s) = unlines $ intersperse hr $ map showRow s
+------------------------------------------------------------------------------
+
+-- * B1
+
+-- | printSudoku sud prints a nice representation of the sudoku sud on
+-- the screen
+printSudoku :: Sudoku -> IO ()
+printSudoku s = mapM_ (putStrLn . map converter) (rows s )
+
+converter :: Maybe Int -> Char
+converter Nothing = '.'
+converter (Just n) = intToDigit n
+
+-- * B2
+
+-- | readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
+readSudoku :: FilePath -> IO Sudoku
+readSudoku file = do
+                     str <- readFile file
+                     let sud = Sudoku $ map (map charToCell) $ lines str
+                     if isSudoku sud
+                       then return sud
+                       else error "Not a Sudoku!"
+
+charToCell :: Char -> Cell
+charToCell '.' = Nothing
+charToCell c = Just (digitToInt c)
+
+------------------------------------------------------------------------------
+
+-- * C1
+
+-- | cell generates an arbitrary cell in a Sudoku
+cell :: Gen Cell
+cell = frequency [(9,return Nothing),
+                  (1, do n <- choose (1,9)
+                         return (Just n))]
+
+-- * C2
+
+-- | an instance for generating Arbitrary Sudokus
+instance Arbitrary Sudoku where
+  arbitrary = do
+          s <-vectorOf 9 $ vectorOf 9 cell
+          return (Sudoku s)
+
+ -- hint: get to know the QuickCheck function vectorOf
+
+-- * C3
+
+prop_Sudoku :: Sudoku -> Bool
+prop_Sudoku = isSudoku
+  -- hint: this definition is simple!
+
+------------------------------------------------------------------------------
+
+type Block = [Cell] -- a Row is also a Cell
+
+-- * D1
+
+-- Checks Block for repetitive numbers
+isOkayBlock :: Block -> Bool
+isOkayBlock b = filter (/= Nothing) b == nub ( filter (/= Nothing) b)
+
+-- * D2
+
+blocks :: Sudoku -> [Block]
+blocks sud = rows sud ++ rowsToColumns  ++ rowsToSquares
+  where rowsToColumns = [[  rows sud !! n !! m | n <- [0..8]] | m <-[0..8]]
+        rowsToSquares = [concatMap (take 3 . drop i) $ take 3 $ drop j $  rows sud | j <- [0,3,6], i<- [0,3,6]]
+
+-- Tests the function 'blocks'
+prop_blocks_lengths :: Sudoku -> Bool
+prop_blocks_lengths sud = length (blocks sud) == 3*9 && nub (map length $ blocks sud) == [9]
+
+-- * D3
+
+-- Checks all blocks in a Sudoku for repetitive numbers in rows, columns and squares
+isOkay :: Sudoku -> Bool
+isOkay sud = all isOkayBlock (blocks sud)
+
+---- Part A ends here --------------------------------------------------------
+------------------------------------------------------------------------------
+---- Part B starts here ------------------------------------------------------
+
+
+-- | Positions are pairs (row,column),
+-- (0,0) is top left corner, (8,8) is bottom left corner
+type Pos = (Int,Int)
+
+-- * E1
+
+blanks :: Sudoku -> [Pos]
+blanks sud =  concat (zipWith (\a b -> [(a, c) |c <- b]) [0..8] rws)
   where
-    hr = replicate (9 * 2 - 1) '-'
-    showRow = intersperse '|' . map showNum
-    showNum 0 = ' '
-    showNum n = head (show n)
+      rws = map (elemIndices Nothing) (rows sud)
 
-ex =
-  Sudoku
-    [ [3, 6, 0, 0, 7, 1, 2, 0, 0],
-      [0, 5, 0, 0, 0, 0, 1, 8, 0],
-      [0, 0, 9, 2, 0, 4, 7, 0, 0],
-      [0, 0, 0, 0, 1, 3, 0, 2, 8],
-      [4, 0, 0, 5, 0, 2, 0, 0, 9],
-      [2, 7, 0, 4, 6, 0, 0, 0, 0],
-      [0, 0, 5, 3, 0, 8, 9, 0, 0],
-      [0, 8, 3, 0, 0, 0, 0, 6, 0],
-      [0, 0, 7, 6, 9, 0, 0, 4, 3]
-    ]
+prop_blanks_allBlanks :: Bool
+prop_blanks_allBlanks = blanks allBlankSudoku == [(n,m)| n <-[0..8], m <- [0..8]]
 
-toJaden :: String -> String
-toJaden = undefined
+-- * E2
+(!!=) :: [a] -> (Int,a) -> [a]
+xs !!= (i,y) | i > length xs -1 || i < 0 = xs
+             | otherwise = take i xs ++ y : drop (i+1) xs
 
-n = "How can mirrors be real if our eyes aren't real"
+-- First checks size then checks the input value
+prop_bangBangEquals_correct :: [Int] -> (Int,Int) -> Bool
+prop_bangBangEquals_correct xs (i,y)  = lengthCheck && inputCheck
+  where lengthCheck = length (xs !!= (i,y)) == length xs
+        inputCheck = null xs || xs !!= (i',y) !! i' == y
+        i' = abs $ i `mod` (length xs)
 
-getEachWord = words
 
-add1 :: Num a => a -> a -> a
-add1 x y = x + y
+-- * E3
+update :: Sudoku -> Pos -> Cell -> Sudoku
+update sud (c, r) newVal = Sudoku newRowVal
+  where newRowVal = rows sud !!= (c, (rows sud !! c) !!= (r, newVal))
 
-mul x y = x * y
 
-ex1 = add1 2 $ mul 2 3
+prop_update_updated :: Sudoku -> Pos -> Cell -> Bool 
+prop_update_updated sud (c,r) newCell = updatedCell  == newCell 
+      where updatedCell = rows (update sud (c', r') newCell) !! c' !! r'
+            (c',r') = (abs $ c `mod` 9, abs $ r `mod` 9)  
 
-ex2 = add1 2 $ mul 2 $ mul 3 $ add1 1 2
+------------------------------------------------------------------------------
 
--- (f . g) (a) f(g(a))
-times2 :: [Int] -> [Int]
-times2 = filter (< 17) . filter (> 12) . map (+ 2) . map (* 2)
 
-duplis :: Eq a => [a] -> [a]
-duplis = map head . group
 
-duplisa :: Eq a => [a] -> [a]
-duplisa arr = map head $ group arr
+-- * F1
 
-regg :: [a] -> [a]
-regg (d : x : y : rest) = rest
+solve :: Sudoku -> Maybe Sudoku
+solve sud | not (isSudoku sud && isOkay sud) = Nothing
+          | otherwise = solveHelper sud (blanks sud)
 
-ex1' :: String -> String
-ex1' s = reverse (take 10 (drop 1 (reverse s)))
+solveHelper :: Sudoku -> [Pos] -> Maybe Sudoku
+solveHelper sud [] = Just sud
+solveHelper sud (x:xs)
+        | null (possibleVals sud x) = Nothing
+        | otherwise = (listToMaybe . catMaybes) [solveHelper (update sud x (Just n)) xs | n <- possibleVals sud x]
 
-rump :: String -> String
-rump s = reverse $ take 10 $ drop 1 $ reverse s
+-- Get all possible values that chosen (i, y) postion can have in current state of game
+possibleVals :: Sudoku -> Pos -> [Int]
+possibleVals sud (i,y) = let updatedSudoku x = update sud (i, y) (Just x)
+                          in  [x | x <- [1..9], isOkay (updatedSudoku x)]
 
-bading s =
-  let x = reverse
-      y = take 10
-      z = drop 1
-      e = reverse s
-   in x $ y $ z $ e
+-- * F2
+readAndSolve :: FilePath -> IO ()
+readAndSolve file = do
+    sud <- readSudoku file
+    maybe (putStrLn "no solution") printSudoku (solve sud)
 
-myLast :: [Int] -> Int
-myLast [] = error "r"
-myLast arr = arr !! (length arr - 1)
-
-myButLast :: [Int] -> Int
-myButLast [] = error "r"
-myButLast arr = arr !! (length arr - 2)
-
-elementAt arr index = arr !! index
-
-myLength [] = 0
-myLength (x : xs) = 1 + myLength xs
-
-myReverse :: [a] -> [a]
-myReverse [] = []
-myReverse [x] = [x]
-myReverse (x : xs) = myReverse xs ++ [x]
-
-isPalindrome arr = arr == myReverse arr
-
-data NestedList a = Elem a | List [NestedList a]
-
-flatten :: NestedList a -> [a]
-flatten (Elem n) = [n]
-flatten (List []) = []
-flatten (List (x : xs)) = flatten x ++ flatten (List xs)
-
-compress s = map head $ group s
-
-pack arr = group arr
-
-encode :: Eq a => [a] -> [(Int, a)]
-encode s = zip (map length $ pack s) (map head $ pack s)
-
-data ListItem a = Single a | Multiple Int a
-  deriving (Show)
-
-encodeModified s = map encodeHelper $ encode s
-  where
-    encodeHelper (1, x) = Single x
-    encodeHelper (n, x) = Multiple n x
-
-decodeModified s = whitespace $ unwords $ map decodeHelper $ s
-  where
-    decodeHelper (Multiple n x) = replicate n x
-    decodeHelper (Single x) = replicate 1 x
-
-w = decodeModified [Multiple 4 'a', Single 'b', Multiple 2 'c', Multiple 2 'a', Single 'd', Multiple 4 'e']
-
-whitespace :: String -> String
-whitespace s = [x | x <- s, x /= ' ']
-
-dupli [] = []
-dupli (x : xs) = x : x : dupli xs
+-- * F3
+isSolutionOf :: Sudoku -> Sudoku -> Bool
+isSolutionOf sud1 sud2 | not (isOkay sud1 && isSudoku sud1 && isFilled sud1) = False
+                       | otherwise = and $ zipWith isEqual ((concat . rows)  sud1) ((concat . rows) sud2)
+                       where
+                         isEqual _ Nothing = True
+                         isEqual r r2 = fromJust r == fromJust r2
+-- * F4
+prop_SolveSound :: Sudoku -> Property
+prop_SolveSound sud = let solution = fromJust $ solve sud
+                          in isOkay sud ==> (solution `isSolutionOf` sud)
